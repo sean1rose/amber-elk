3805147c0b35856b34c7814622588f86a5d017a0
Rishi Goomar <rishig92@gmail.com>
2014-10-16 15:28:20 -0700

    Added mocha test to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,24 @@
 var gulp = require('gulp');
 var bs = require('browser-sync');
 var nodemon = require('gulp-nodemon');
-
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var mocha = require('gulp-mocha');
 // the paths to our app files
 var paths = {
   scripts: ['client/app/**/*.js'],
   html: ['client/pages/*.html', 'client/index.html'],
   styles: ['client/styles/**/*.css'],
-  test: ['tests/**/*.js']
+  tests: ['tests/**/*.js'],
+  serverScripts: ['server/**/*.js']
 };
+
+// JS HINT on both client js and server js
+gulp.task('lint', function() {
+  return gulp.src(paths.scripts.concat(paths.serverScripts))
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
 
 // Start browser sync with the server
 gulp.task('start', ['serve'],function () {
@@ -26,8 +36,13 @@ gulp.task('serve', function() {
   nodemon({script: 'server/index.js', ignore: 'node_modules/**/*.js'});
 });
 
-// TODO add testing task
-
+// Mocha testing
+gulp.task('test', function() {
+  return gulp.src(paths.tests, {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
+});
 // TODO add minifying tasks for JS and CSS
 
-gulp.task('default', ['start']);
+// TODO generate JSDOC
+
+gulp.task('default', ['lint', 'test', 'start']);
