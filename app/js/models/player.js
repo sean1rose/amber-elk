06d5c9b1a33a8bd9d0d5f0092c562a61304ab879
Tom G Varik <tgvarik@me.com>
2014-10-16 11:22:53 -0700

    Empty class

---
+++ app/js/models/player.js
@@ -0,0 +1,17 @@
+/**
+ * Creates a new PlayerCharacter object ready to add to the scene.
+ * @class
+ * @classdesc A wrapper for the various mesh objects that comprise the player
+ *     character model and its associated logic.
+ * @params Creation parameters
+ */
+var PlayerCharacter = function (params) {
+
+};
+
+/**
+ * Raises the player level by one and adjust geometry accordingly.
+ */
+PlayerCharacter.prototype.levelUp = function(){
+
+};
