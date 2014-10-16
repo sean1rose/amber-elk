28363730121525a2c0c36e246a2e30f4aaec5638
Rishi Goomar <rishig92@gmail.com>
2014-10-16 15:28:20 -0700

    Added mocha test to gulp

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 // Serve static files
-app.use("/", express.static(path.join(__dirname + '/../client')));
-app.use("/styles", express.static(path.join(__dirname + '/../client/styles')));
+app.use('/', express.static(path.join(__dirname + '/../client')));
+app.use('/styles', express.static(path.join(__dirname + '/../client/styles')));
 // Listen on port 1337
 app.listen(1337);
