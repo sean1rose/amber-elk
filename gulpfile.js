e40b2c02ec84886e648d026d8d63b257da1becef
Rishi Goomar <rishig92@gmail.com>
2014-10-20 13:11:46 -0700

    Changed deployment and build process

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,23 @@ var jsdoc = require('gulp-jsdoc');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var cssmin = require('gulp-cssmin');
-var rename  = require("gulp-rename");
+var rename  = require('gulp-rename');
+var rimraf = require('gulp-rimraf');
 // the paths to our app files
 var paths = {
   scripts: ['client/app/**/*.js'],
+  distscripts: ['dist/js/**/*.js'],
   html: ['client/pages/*.html', 'client/index.html'],
+  disthtml: ['dist/pages/*.html', 'dist/index.html'],
   styles: ['client/styles/**/*.css'],
+  diststyles: ['dist/css/**/*.css'],
   tests: ['tests/**/*.js'],
   serverScripts: ['server/**/*.js']
 };
 
 // JS HINT on both client js and server js
 gulp.task('lint', function() {
-  return gulp.src(paths.scripts.concat(paths.serverScripts))
+  return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
 });
@@ -31,7 +35,7 @@ gulp.task('start', ['serve'],function () {
   bs({
     notify: true,
     injectChanges: true,
-    files: paths.scripts.concat(paths.html, paths.styles),
+    files: paths.scripts.concat(paths.disthtml, paths.diststyles),
     proxy: 'localhost:1337'
   });
 });
@@ -63,8 +67,48 @@ gulp.task('mincss', function() {
     .pipe(cssmin())
     .pipe(gulp.dest('dist/css'));
 });
+// Get external libraries
+gulp.task('bowerbuildjs', function() {
+  gulp.src([
+      'client/bower_components/jquery/dist/jquery.min.js',
+      'client/bower_components/firebase/firebase.js',
+      'client/bower_components/three.js/three.min.js',
+      'client/bower_components/semantic/build/packaged/javascript/semantic.min.js'
+    ])
+    .pipe(uglify({ mangle: false, compress: false }))
+    .pipe(concat('vendor.js'))
+    .pipe(rename({suffix: '.min' }))
+    .pipe(gulp.dest('dist/js'));
+});
+gulp.task('bowerbuildcss', function() {
+  gulp.src([
+      'client/bower_components/semantic/build/packaged/css/semantic.min.css'
+    ])
+    .pipe(concat('vendor.css'))
+    .pipe(rename({suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
+});
+// Grab other static files
+gulp.task('copystatic', function() {
+  gulp.src(['client/index.html'])
+    .pipe(gulp.dest('dist'));
+  gulp.src(['client/pages/**/*.html'])
+    .pipe(gulp.dest('dist/pages'));
+  gulp.src(['client/public/**'])
+    .pipe(gulp.dest('dist/public'));
+  gulp.src(['client/bower_components/semantic/build/packaged/fonts/**.*'])
+    .pipe(gulp.dest('dist/fonts'));
+});
+// Remove the dist folder to properly regenerate it
+gulp.task('cleanup', function() {
+  gulp.src(['dist'])
+    .pipe(rimraf());
+});
 // Build for production
-gulp.task('build', ['minjs', 'mincss']);
+gulp.task('build', ['cleanup', 'copystatic', 'bowerbuildjs', 'bowerbuildcss', 'minjs', 'mincss']);
+
+// Get ready for deployment
+gulp.task('deploy', ['lint', 'build', 'test']);
 
 // Generate docs using jsdoc
 gulp.task('docs', function() {
@@ -72,4 +116,4 @@ gulp.task('docs', function() {
     .pipe(jsdoc('./docs'));
 });
 // By default, run linter, test the code, and start the server
-gulp.task('default', ['lint', 'test', 'start']);
+gulp.task('default', ['lint', 'build', 'test', 'start']);
