602543ccdb965e3244927147ac8988685f73ff49
Liam Schauerman <ljschauerman@gmail.com>
2014-10-23 14:22:27 -0700

    collisions detected

--- demo-scene/cameratest.js
+++ demo-scene/cameratest.js
@@ -1,31 +1,4 @@
 
-// var moveCamera = function(axis, distance) {
-// 	camera.position[axis] += distance;
-// }
-var movePlayer = function(axis, distance) {
-	player.position[axis] += distance;
-	camera.position[axis] += (distance * 1.3);
-}
-
-
-document.onkeydown = function(e) {
-	switch (e.keyCode) {
-    case 37: // left
-      movePlayer('x', -10)
-      break;
-    case 38: // up
-      movePlayer('y', 10)
-      break;
-    case 39: // right
-      movePlayer('x', 10)
-      break;
-    case 40: // down
-      movePlayer('y', -10)
-      break;
-	}
-	// console.log(camera.position, player.position)
-  camera.lookAt(new THREE.Vector3(0,0,0))
-};
 
 document.onmousemove = function (e) {mousePos(e);};
 var mouseX = 0;
@@ -38,7 +11,3 @@ function mousePos (e) {
 
     return true;
 }
-
-
-// controls = new THREE.OrbitControls( camera );
-// controls.target.set( 0, 0, 0 )
--- demo-scene/spheretest.js
+++ demo-scene/spheretest.js
@@ -1,5 +1,6 @@
 // creating the scene. 
 var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.shadowMapEnabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
@@ -13,25 +14,73 @@ var makeCubeAtOrigin = function(){
   cube.position.set(0,0,0)
 }();
 
+// var ambientLight = new THREE.AmbientLight(000080);
+// scene.add(ambientLight);
+
+
 // setting the player
-var geometry = new THREE.RingGeometry(10, 15, 18);
-var material = new THREE.MeshLambertMaterial({color: 0xff0000, transparent: true, opacity: 0.9});
-var player = new THREE.Mesh(geometry, material);
-player.level = 1;
-player.position.set(0,0,200);
-scene.add(player);
+var createPlayer = function(innerRadius, outerRadius, x, y, z){
+  x ? x = x : x = 0;
+  y ? y = y : y = 0;
+  z ? z = z : z = 200;
+  var innerRadius = innerRadius || 10;
+  var outerRadius = outerRadius || 15;
+  var geometry = new THREE.RingGeometry(innerRadius, outerRadius, 18);
+  var material = new THREE.MeshLambertMaterial({color: 0xD1FF00, transparent: true, opacity: 0.9});
+  var player = new THREE.Mesh(geometry, material);
+  player.radius = outerRadius;
+  player.position.set(0,0,200);
+  return player
+}
+var player = createPlayer(10, 15);
+scene.add( player );
+
+// //lighting
+var light = new THREE.DirectionalLight(000080, 000080, 0.99);
+light.position.set(-900,900,230);
+light.intensity = .1;
+scene.add(light);
+
+var directionalLight = new THREE.DirectionalLight(0xffffff);
+directionalLight.intensity = .7
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
+
+var playerLight = new THREE.SpotLight( 0xffffff );
+playerLight.position.set( 50, 90, 1000 );
+scene.add( playerLight );
 
 
-// plane as the ocean floor
-var plane = new THREE.Mesh(new THREE.PlaneGeometry(400, 400), new THREE.MeshNormalMaterial());
-plane.overdraw = true;
-// plane.rotation.x = Math.PI
-plane.position.y = -200;
-plane.position.z = 10
-scene.add(plane);
+/*var spotLight = new THREE.SpotLight( 0xffffff );
+spotLight.position.set( 50, 90, 200 );
+spotLight.castShadow = true;
+
+spotLight.shadowCameraVisible = true;
+var d = 200;
+
+spotLight.shadowCameraLeft = -d;
+spotLight.shadowCameraRight = d;
+spotLight.shadowCameraTop = d;
+spotLight.shadowCameraBottom = -d;
+
+spotLight.shadowMapWidth = 512;
+spotLight.shadowMapHeight = 512;
+spotLight.shadowCameraNear = 1;
+spotLight.shadowCameraFar = 4000;
+// spotLight.shadowCameraFov = 30;
+scene.add( spotLight );*/
+
+//setting the ground
+var floor;
+var geometry = new THREE.BoxGeometry( 900, 10, 100000 );
+var material = new THREE.MeshLambertMaterial( {color: 0x0FF00} );
+floor = new THREE.Mesh( geometry, material );
+floor.position.set(0, -100, -300)
+floor.receiveShadow = true;
+scene.add( floor );
 
 //setting the camera
-var camera = new THREE.PerspectiveCamera (50, window.innerWidth / window.innerHeight, 5, 800);
+var camera = new THREE.PerspectiveCamera (50, window.innerWidth / window.innerHeight, 5, 2800);
 camera.position.set(player.position.x, 50, player.position.z*2);
 camera.lookAt(scene.position);
 
@@ -45,7 +94,7 @@ var particles = new THREE.Geometry();
 var makeParticle = function(particle){
   var px = (Math.random() * window.innerWidth) - (window.innerWidth / 2);
   var py = (Math.random() * window.innerHeight) - (window.innerHeight / 2);
-  var pz = 200*1.3;
+  var pz = -500;
   var vx = Math.random();
   var vy = Math.random();
   var vz = 4 + Math.random();
@@ -75,15 +124,15 @@ var sphereCount = 20;
 var Sphere = function(min, max) {
   var sx = .15*((Math.random() * window.innerWidth) - (window.innerWidth / 2));
   var sy = .15*((Math.random() * window.innerHeight) - (window.innerHeight / 2));
-  var sz = Math.random() * player.position.z - player.position.z;  
+  var sz = (Math.random() * player.position.z) - player.position.z;  
   var r = Math.random()*max;
-  console.log(r)
   var edge1 = Math.random()*5 + 5;
   var edge2 = Math.random()*5 + 5;
-  sphere = new THREE.Mesh(new THREE.SphereGeometry(r, edge1, edge2), new THREE.MeshNormalMaterial());
+  sphere = new THREE.Mesh(new THREE.SphereGeometry(r, edge1, edge2), new THREE.MeshLambertMaterial({color: 0x0aeedf}));
   sphere.overdraw = true;
   scene.add(sphere);
-  sphere.position.set(sx, sy, sz)
+  sphere.position.set(sx, sy, sz);
+  sphere.radius = r;
   return sphere;
 }
 var makeSpheres = function(){
@@ -95,33 +144,36 @@ var makeSpheres = function(){
 }
 var spheres = makeSpheres();
 
-var ambientLight = new THREE.AmbientLight(000080);
-scene.add(ambientLight);
+// calculate distance between player and enemy
+var findDistance = function(obj1, obj2) {
+  var dx = obj1.position.x - obj2.position.x;
+  var dy = obj1.position.y - obj2.position.y;
+  return Math.sqrt(Math.pow(dx, 2)+Math.pow(dy, 2))
+}
 
-// var light = new THREE.HemisphereLight(000080, 000080, 0.99);
-// light.position.set(0,0,230);
-// scene.add(light);
-
+var checkCollision = function(s){
+  if( spheres[s].position.z > (player.position.z - spheres[s].radius) && spheres[s].position.z < (player.position.z + spheres[s].radius) ){ //check each enemy's position against the players position
+    // console.log(spheres[s].position.z)
+  // console.log(player.position.z)
+    var d = findDistance(player, spheres[s])
+    if(d < player.radius){
+      console.log('Player radius ++!!!!')
+      console.log
+    }
+  }
+}
 
 var update = function(){
+  var timer = Date.now() * 0.0002;
   particleSystem.rotation.z += 0.001;
-  // particleSystemNeon.rotation.z += 0.8;
-  var pCount = particleCount
-  // while (pCount--){
-  //   var particle = particles.vertices[pCount];
-  //   if (particle.z > camera.position.z || particle.y > window.innerHeight/2 || particle.y < -window.innerHeight/2 || particle.x > window.innerWidth/2 || particle.x < -window.innerWidth/2){
-  //     makeParticle(particle);
-  //   }
-  //   particle.add(particle.velocity);
-  // }
   particleSystem.geometry.__dirtyVertices = true;
   for (var s = 0; s < spheres.length; s++) {
-  	if( spheres[s].position.z > camera.position.z) {
-  		spheres[s].position.z = -710;
+    checkCollision(s);
+  	if( spheres[s].position.z > player.position.z+75) {
+  		spheres[s].position.z = -750;
   	}
   	spheres[s].position.z += 2;
   };
-  plane.rotation.x += .01
   renderer.render(scene, camera);
   requestAnimationFrame(update);
 };
