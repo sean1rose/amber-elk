600e4801b5fd044c7914895e22c754cd0d7df472
Liam Schauerman <ljschauerman@gmail.com>
2014-10-24 16:28:59 -0700

    rough game ready for live testing

--- demo-scene/cameratest.js
+++ demo-scene/cameratest.js
@@ -1,13 +1,12 @@
 
 
+
 document.onmousemove = function (e) {mousePos(e);};
 var mouseX = 0;
 var mouseY = 0;
 function mousePos (e) {
-        mouseX = e.pageX; 
-        mouseY = e.pageY;
-    player.position.set((mouseX-window.innerWidth/2)*.25, .25*(-(mouseY-window.innerHeight/2)), player.position.z)
-    // console.log(player.position.x, player.position.y)
-
-    return true;
+	mouseX = e.pageX; 
+	mouseY = e.pageY;
+	player.position.set((mouseX-window.innerWidth/2)*.25, .25*(-(mouseY-window.innerHeight/2)), player.position.z)
+	return true;
 }
